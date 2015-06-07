9e74245cfbdaf9e3a8af45e55a711dc3adba1ea2
Kevin Robatel <kevinrob2@gmail.com>
2015-06-07 18:09:30 +0200

    Add TODO for stale-if-error caching

--- src/CacheMiddleware.php
+++ src/CacheMiddleware.php
@@ -20,12 +20,17 @@ class CacheMiddleware
         return function (callable $handler) {
             return function ($request, array $options) use ($handler) {
                 // TODO Add logic here for HTTP Caching
-                // If cache => return new FulfilledPromise with response
+                // If cache => return new FulfilledPromise(...) with response
 
                 /** @var Promise $promise */
                 $promise = $handler($request, $options);
                 return $promise->then(
                     function (ResponseInterface $response) use (&$cache) {
+                        if ($response->getStatusCode() >= 500) {
+                            // TODO Check if we have a stale response to serve (stale-if-error)
+                            // return new FulfilledPromise(...);
+                        }
+
                         // TODO Add logic for adding response to cache
 
                         return $response;
