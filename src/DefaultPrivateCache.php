aba12ed61758ea26aa3c672eafde1d216d0470a9
Kevin Robatel <kevinrob2@gmail.com>
2015-06-21 17:08:51 +0200

    Create default private cache

---
+++ src/DefaultPrivateCache.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Kevin
+ * Date: 21.06.2015
+ * Time: 16:50
+ */
+
+namespace Kevinrob\GuzzleCache;
+
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+class DefaultPrivateCache extends AbstractPrivateCache
+{
+    /**
+     * @var CacheEntry[]
+     */
+    protected $cache = [];
+
+
+    /**
+     * Return a CacheEntry or null if no cache.
+     *
+     * @param RequestInterface $request
+     * @return CacheEntry|null
+     */
+    public function fetch(RequestInterface $request)
+    {
+        $key = $this->getCacheKey($request);
+        if (isset($this->cache[$key])) {
+            return $this->cache[$key];
+        }
+
+        return null;
+    }
+
+    /**
+     * @param RequestInterface $request
+     * @param ResponseInterface $response
+     * @return bool true if success
+     */
+    public function cache(RequestInterface $request, ResponseInterface $response)
+    {
+        $entry = $this->getCacheObject($response);
+        if ($entry != null) {
+            $this->cache[$this->getCacheKey($request)] = $entry;
+        }
+    }
+
+    /**
+     * @param RequestInterface $request
+     * @return string
+     */
+    protected function getCacheKey(RequestInterface $request)
+    {
+        return sha1(
+            $request->getMethod() . $request->getUri()
+        );
+    }
+}
