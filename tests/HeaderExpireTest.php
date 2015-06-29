5f5c9f81c8ce634e653c69346f022821574d475a
Kevin Robatel <kevinrob2@gmail.com>
2015-06-29 22:57:20 +0200

    Bootstrap tests

---
+++ tests/HeaderExpireTest.php
@@ -0,0 +1,46 @@
+<?php
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Promise\FulfilledPromise;
+use GuzzleHttp\Psr7\Response;
+use Kevinrob\GuzzleCache\CacheMiddleware;
+use Psr\Http\Message\RequestInterface;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Kevin
+ * Date: 29.06.2015
+ * Time: 22:48
+ */
+class HeaderExpireTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    public function setUp()
+    {
+        // Create default HandlerStack
+        $stack = HandlerStack::create(function(RequestInterface $request, array $options) {
+            return new FulfilledPromise(
+                (new Response())
+                    ->withHeader("Expires", gmdate('D, d M Y H:i:s T', time() + 2))
+            );
+        });
+
+        // Add this middleware to the top with `push`
+        $stack->push(CacheMiddleware::getMiddleware(), 'cache');
+
+        // Initialize the client with the handler option
+        $this->client = new Client(['handler' => $stack]);
+    }
+
+    public function testMockedResponse()
+    {
+        $response = $this->client->get("anything");
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+}
