c48e3eae5f0853de13e45b2526fd266499d4cf99
Kevin Robatel <kevinrob2@gmail.com>
2015-08-08 14:54:43 +0200

    Change HTTP code cacheability (rfc7231#section-6.1)

--- src/PrivateCache.php
+++ src/PrivateCache.php
@@ -21,12 +21,16 @@ class PrivateCache implements CacheStorageInterface
      */
     protected $statusAccepted = [
         200 => 200,
+        203 => 203,
         204 => 204,
-        400 => 400,
+        300 => 300,
+        301 => 301,
         404 => 404,
         405 => 405,
+        410 => 410,
         414 => 414,
         418 => 418,
+        501 => 501,
     ];
 
     public function __construct(Cache $cache = null)
