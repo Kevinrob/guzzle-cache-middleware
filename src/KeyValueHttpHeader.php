0fd028d18038d725e9efe95ef1d44cc4fe8d24d0
Kevin Robatel <k.robatel@edsi-tech.com>
2015-11-13 16:00:34 +0100

    Implement Iterator for KeyValueHttpHeader

--- src/KeyValueHttpHeader.php
+++ src/KeyValueHttpHeader.php
@@ -2,7 +2,7 @@
 
 namespace Kevinrob\GuzzleCache;
 
-class KeyValueHttpHeader
+class KeyValueHttpHeader implements \Iterator
 {
     /**
      * Take from https://github.com/hapijs/wreck.
@@ -68,5 +68,61 @@ class KeyValueHttpHeader
     public function isEmpty()
     {
         return count($this->values) === 0;
+    }
+
+    /**
+     * Return the current element
+     * @link http://php.net/manual/en/iterator.current.php
+     * @return mixed Can return any type.
+     * @since 5.0.0
+     */
+    public function current()
+    {
+        return current($this->values);
+    }
+
+    /**
+     * Move forward to next element
+     * @link http://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function next()
+    {
+        next($this->values);
+    }
+
+    /**
+     * Return the key of the current element
+     * @link http://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     * @since 5.0.0
+     */
+    public function key()
+    {
+        return key($this->values);
+    }
+
+    /**
+     * Checks if current position is valid
+     * @link http://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     * @since 5.0.0
+     */
+    public function valid()
+    {
+        return key($this->values) !== null;
+    }
+
+    /**
+     * Rewind the Iterator to the first element
+     * @link http://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function rewind()
+    {
+        reset($this->values);
     }
 }
