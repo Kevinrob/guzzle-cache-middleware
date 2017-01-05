0322da4c86aa762eaa44c13f9cc28af3b2c9652a
Damian Posener <damian@thestudentroom.com>
2017-01-05 11:04:40 +0000

    Prevent meta-headers from being included in cache

--- src/CacheMiddleware.php
+++ src/CacheMiddleware.php
@@ -202,8 +202,8 @@ class CacheMiddleware
                         $response = $response->withBody($cacheEntry->getResponse()->getBody());
 
                         // Merge headers of the "304 Not Modified" and the cache entry
-                        foreach ($cacheEntry->getResponse()->getHeaders() as $headerName => $headerValue) {
-                            if (!$response->hasHeader($headerName)) {
+                        foreach ($cacheEntry->getOriginalResponse()->getHeaders() as $headerName => $headerValue) {
+                            if (!$response->hasHeader($headerName) and $headerName !== self::HEADER_CACHE_INFO) {
                                 $response = $response->withHeader($headerName, $headerValue);
                             }
                         }
