55ef8b23337395e73d3b4a21cdedb5067b56790d
Phil Moorhouse <phil.moorhouse02.ext@bbc.co.uk>
2017-02-02 12:18:33 +0000

    cache now respects vary-headers

--- tests/ResponseVaryTest.php
+++ tests/ResponseVaryTest.php
@@ -44,7 +44,7 @@ class ResponseVaryTest extends \PHPUnit_Framework_TestCase
         $this->client = new Client(['handler' => $stack]);
     }
 
-    public function testVaryAllHeader()
+    public function testVaryAllHeaderDoesNotCache()
     {
         $this->client->get('http://test.com/vary-all');
 
@@ -90,6 +90,76 @@ class ResponseVaryTest extends \PHPUnit_Framework_TestCase
         $response = $this->client->get('http://test.com/vary-my-header');
         $this->assertEquals(
             CacheMiddleware::HEADER_CACHE_MISS,
+            $response->getHeaderLine(CacheMiddleware::HEADER_CACHE_INFO)
+        );
+    }
+
+    public function testVaryHeadersHaveUniqueCache()
+    {
+        $headerSetA = [
+            'My-Header' => 'hello'
+        ];
+
+        $headerSetB = [
+            'My-Header' => 'goodbye'
+        ];
+
+        $this->client->get('http://test.com/vary-my-header', [
+            'headers' => $headerSetA
+        ]);
+
+        // Second request with A headers - cache hit
+        $response = $this->client->get('http://test.com/vary-my-header', [
+            'headers' => $headerSetA
+        ]);
+        $this->assertEquals(
+            CacheMiddleware::HEADER_CACHE_HIT,
+            $response->getHeaderLine(CacheMiddleware::HEADER_CACHE_INFO)
+        );
+
+        // First request with B headers - cache miss
+        $response = $this->client->get('http://test.com/vary-my-header', [
+            'headers' => $headerSetB
+        ]);
+        $this->assertEquals(
+            CacheMiddleware::HEADER_CACHE_MISS,
+            $response->getHeaderLine(CacheMiddleware::HEADER_CACHE_INFO)
+        );
+
+        // Second request with B headers - cache hit
+        $response = $this->client->get('http://test.com/vary-my-header', [
+            'headers' => $headerSetB
+        ]);
+        $this->assertEquals(
+            CacheMiddleware::HEADER_CACHE_HIT,
+            $response->getHeaderLine(CacheMiddleware::HEADER_CACHE_INFO)
+        );
+
+        // Third request with A headers - cache hit (make sure B cache did not overwrite A cache)
+        $response = $this->client->get('http://test.com/vary-my-header', [
+            'headers' => $headerSetA
+        ]);
+        $this->assertEquals(
+            CacheMiddleware::HEADER_CACHE_HIT,
+            $response->getHeaderLine(CacheMiddleware::HEADER_CACHE_INFO)
+        );
+    }
+
+    public function testVaryHeaderCaseChangeDoesNotCauseCacheMiss()
+    {
+        $this->client->get('http://test.com/vary-my-header', [
+            'headers' => [
+                'My-Header' => 'hello'
+            ]
+        ]);
+
+        $response = $this->client->get('http://test.com/vary-my-header', [
+            'headers' => [
+                'my-header' => 'hello'
+            ]
+        ]);
+        $this->assertEquals(
+            CacheMiddleware::HEADER_CACHE_HIT,
             $response->getHeaderLine(CacheMiddleware::HEADER_CACHE_INFO)
         );
     }
