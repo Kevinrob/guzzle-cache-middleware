545fde9f653f64fca067d8c40c852535d2074c3d
Beno!t POLASZEK <bpolaszek@gmail.com>
2017-07-07 14:46:42 +0200

    NullCache

---
+++ tests/NullCacheTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Kevinrob\GuzzleCache;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use Kevinrob\GuzzleCache\Strategy\NullCacheStrategy;
+use PHPUnit\Framework\TestCase;
+
+class NullCacheTest extends TestCase
+{
+
+    public function testCacheIsNeverHit()
+    {
+        $stack = HandlerStack::create(new MockHandler([
+            new Response(),
+            new Response(),
+        ]));
+
+        $stack->push(new CacheMiddleware(new NullCacheStrategy()));
+
+        $request = new Request('GET', '/foo');
+
+        $client = new Client([
+            'handler' => $stack
+        ]);
+        $client->send($request); // Will not be cached
+        $response = $client->send($request); // Will not come from cache
+
+        $this->assertEquals(CacheMiddleware::HEADER_CACHE_MISS, $response->getHeaderLine(CacheMiddleware::HEADER_CACHE_INFO));
+    }
+
+}
