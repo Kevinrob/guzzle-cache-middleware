684d321c00871c67bf90debe001826e90189f8a3
Marcus <info@marcus-obst.de>
2021-02-03 16:15:40 +0100

    Add Flysystem 2 Storage
    Just upgraded to V2 https://flysystem.thephpleague.com/v2/docs/advanced/upgrade-to-2.0.0/
    
    `use League\Flysystem\AdapterInterface;` => `use League\Flysystem\Local\LocalFilesystemAdapter;`
    
    `$this->filesystem->has` => `$this->filesystem->fileExists`
    `$this->filesystem->put` => `$this->filesystem->write`

---
+++ src/Storage/Flysystem2Storage.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Kevinrob\GuzzleCache\Storage;
+
+use Kevinrob\GuzzleCache\CacheEntry;
+use League\Flysystem\Local\LocalFilesystemAdapter;
+use League\Flysystem\Filesystem;
+use League\Flysystem\FileNotFoundException;
+
+class Flysystem2Storage implements CacheStorageInterface
+{
+
+    /**
+     * @var Filesystem
+     */
+    protected $filesystem;
+
+    public function __construct(LocalFilesystemAdapter $adapter)
+    {
+        $this->filesystem = new Filesystem($adapter);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function fetch($key)
+    {
+        if ($this->filesystem->fileExists($key)) {
+            // The file exist, read it!
+            $data = @unserialize(
+                $this->filesystem->read($key)
+            );
+
+            if ($data instanceof CacheEntry) {
+                return $data;
+            }
+        }
+
+        return;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function save($key, CacheEntry $data)
+    {
+        return $this->filesystem->write($key, serialize($data));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function delete($key)
+    {
+        try {
+            return $this->filesystem->delete($key);
+        } catch (FileNotFoundException $ex) {
+            return true;
+        }
+    }
+}
