c31765c123d625e20e0b2f4a982b75f5a07466b7
Kevin Robatel <kevinrob2@gmail.com>
2024-09-05 08:59:56 +0000

    Fix GreedyCacheStrategy with negative TTL

--- src/Strategy/GreedyCacheStrategy.php
+++ src/Strategy/GreedyCacheStrategy.php
@@ -100,7 +100,9 @@ class GreedyCacheStrategy extends PrivateCacheStrategy
             $ttl = (int)reset($ttlHeaderValues);
         }
 
-        return new CacheEntry($request->withoutHeader(static::HEADER_TTL), $response, new \DateTime(sprintf('+%d seconds', $ttl)));
+        $sign = $ttl > 0 ? '+' : '-';
+
+        return new CacheEntry($request->withoutHeader(static::HEADER_TTL), $response, new \DateTime(sprintf($sign.'%d seconds', $ttl)));
     }
 
     public function fetch(RequestInterface $request)
